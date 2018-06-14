fcf64215294f4816285c055011bcf0a35c0dbbf7
rpluu <ryanpluu@gmail.com>
2018-06-14 03:19:38 -0700

    Week 1 assignments

---
+++ week1/ch05/bigger_better_favorite_number.rb
@@ -0,0 +1,5 @@
+puts 'Excuse me. What is your favorite number?'
+number = gets.chomp
+better = number.to_i + 1
+puts 'Wow, good choice. But, I think the number ' + better.to_s + ' might be a bit better.'
+puts 'Don\'t you think?'
---
+++ week1/ch05/full_name_greeting.rb
@@ -0,0 +1,8 @@
+qt = 'What\'s your '
+puts qt + 'first name?'
+fname = gets.chomp
+puts qt + 'middle name?'
+mname = gets.chomp
+puts qt + 'last name?'
+lname = gets.chomp
+puts 'Why, hello there, ' + fname + ' ' + mname + ' ' + lname
